564b0af3155123294ad9b13a2ed6cf3aaf06d673
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-09-18 20:39:31 +0200

    fix: small function update

--- pages/api/import/index.ts
+++ pages/api/import/index.ts
@@ -46,7 +46,7 @@ export default async function handler(
         }
       );
 
-      res.status(200).send({ response: response });
+      res.status(200).send({ response });
     } catch (err) {
       res.status(500).send({ error: "failed", err });
     }
