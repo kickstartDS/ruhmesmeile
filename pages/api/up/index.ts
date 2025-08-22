1572326b2224fe2ea7e8f124fcc109d9d0aea4c5
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2025-08-22 15:37:09 +0200

    fix: update WIP

--- pages/api/up/index.ts
+++ pages/api/up/index.ts
@@ -1,34 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import StoryblokClient from "storyblok-js-client";
-import Cors from "cors";
-
-const cors = Cors({
-  methods: ["GET"],
-});
-
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    await runMiddleware(req, res, cors);
-
     return res.status(200).send("Ok");
   } else {
     return res.status(405).send({ error: "method not allowed" });
