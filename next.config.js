e9be299b98c673ba340987e2f4ebe982deae4805
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2025-08-22 15:21:21 +0200

    feature: add WIP

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const nextConfig = {
     "@kickstartds/form",
     "@kickstartds/ds-agency-premium",
   ],
+  output: "standalone",
 };
 
 module.exports = {
@@ -41,13 +42,13 @@ module.exports = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/c15t/:path*",
-        destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/c15t/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
+  //     },
+  //   ];
+  // },
   ...nextConfig,
 };
